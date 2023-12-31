f1e4ff64d5f26165c72908b40f864f61082d8fcb
juanvidev <juanvidev1@gmail.com>
2023-12-31 15:35:23 -0500

    feat: Add prettier file

--- react-basis/src/app/AppUI.js
+++ react-basis/src/app/AppUI.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { Title } from '../atoms/Header';
+import { Title } from "../atoms/Header";
 import { Paragraph } from "../atoms/Paragraph";
 import { Image } from "../atoms/Image";
-import profilePic from '../assets/images/ProfilePicture.JPG';
+// import profilePic from '../assets/images/ProfilePicture.JPG';
 
 const AppUI = () => {
-    return (
-        <div className="main-container">
-            <Title text={"Inicio"} />
-            <Paragraph text={"Bienvenido"} />
-            <Image 
-            imgLink={profilePic} 
-            altText={"Foto de JuanviDev"} 
-            figClassName={"img-cont"} 
-            figcaption={"JuanviDev"} />
-        </div>
-    );
+  return (
+    <div className="main-container">
+      <Title text={"Inicio"} />
+      <Paragraph text={"Bienvenido"} />
+      <Image
+        imgLink={
+          "https://images.unsplash.com/photo-1703756292790-884c3d635e79?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8"
+        }
+        altText={"Foto de gorila"}
+        figClassName={"img-cont"}
+        figcaption={"Ape image - Imagen de un gorila"}
+      />
+    </div>
+  );
 };
 
 export { AppUI };
